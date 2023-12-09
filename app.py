fa2d3a25f8260ce0cc21000e8dd83806e6ad460c
Ashpreet Singh <84430991+ashpreettsinghh@users.noreply.github.com>
2023-12-09 14:56:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,11 +27,11 @@ user_input = {}
 # Add input fields to the sidebar
 user_input['CreditScore'] = st.sidebar.slider('Credit Score', min_value=300, max_value=850, value=700)
 user_input['Age'] = st.sidebar.slider('Age', min_value=18, max_value=100, value=30)
-user_input['Tenure'] = st.sidebar.slider('Tenure', min_value=0, max_value=10, value=5)
+user_input['Tenure'] = st.sidebar.slider('Tenure (in years) ', min_value=0, max_value=10, value=5)
 user_input['Balance'] = st.sidebar.number_input('Balance', min_value=0.0, value=0.0)
 user_input['NumOfProducts'] = st.sidebar.slider('Number of Products', min_value=1, max_value=4, value=1)
-user_input['HasCrCard'] = st.sidebar.selectbox('Has Credit Card', [0, 1])
-user_input['IsActiveMember'] = st.sidebar.selectbox('Is Active Member', [0, 1])
+user_input['HasCrCard'] = st.sidebar.selectbox('Has Credit Card (Yes=1, No=0) ', [0, 1])
+user_input['IsActiveMember'] = st.sidebar.selectbox('Is Active Member (Yes=1, No=0)', [0, 1])
 user_input['EstimatedSalary'] = st.sidebar.number_input('Estimated Salary', min_value=0.0, value=50000.0)
 user_input['Male'] = st.sidebar.selectbox('Gender (Male=1, Female=0)', [0, 1])
 
